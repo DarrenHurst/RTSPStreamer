e1eacbf2dfa52e7765ee7f66a526d32c98636c03
Darren Hurst <hurst_darren@hotmail.com>
2022-12-29 16:36:43 -0500

    Initial Commit

---
+++ RTSPStreamer/RTSPStreamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RTSPStreamerApp.swift
+//  RTSPStreamer
+//
+//  Created by Darren Hurst on 2022-12-29.
+//
+
+import SwiftUI
+
+@main
+struct RTSPStreamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
